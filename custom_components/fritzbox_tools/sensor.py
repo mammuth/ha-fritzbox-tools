6328dfa80811a5c8ad90fb27f4e31610030d2ccc
Max Muth <kontakt@maxi-muth.de>
2019-09-17 16:34:09 +0200

    Increase connectivity sensor update interval

--- custom_components/fritzbox_tools/sensor.py
+++ custom_components/fritzbox_tools/sensor.py
@@ -7,7 +7,7 @@ from . import DOMAIN, DATA_FRITZ_TOOLS_INSTANCE
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(seconds=5)
+SCAN_INTERVAL = timedelta(seconds=60)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
