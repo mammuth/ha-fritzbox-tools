843c098d17e658c83b0d7333092d2bfb5c26d01c
Simone Chemelli <simone.chemelli@gmail.com>
2021-03-17 19:27:02 +0100

    Clean up part 1 - __init__ (#152)
    * Clean up part 1 - __init__
    
    * Minor enanchements
    
    * Fix after resolving conflict

--- custom_components/fritzbox_tools/__init__.py
+++ custom_components/fritzbox_tools/__init__.py
@@ -1,13 +1,9 @@
-"""Support for AVM Fritz!Box functions"""
-import asyncio
+"""Support for AVM Fritz!Box functions."""
 import logging
 import socket
-import time
 
 import voluptuous as vol
 
-import homeassistant.helpers.config_validation as cv
-from homeassistant import config_entries
 from homeassistant.config_entries import SOURCE_IMPORT, SOURCE_REAUTH, ConfigEntry
 from homeassistant.const import (
     CONF_DEVICES,
@@ -16,36 +12,34 @@ from homeassistant.const import (
     CONF_PORT,
     CONF_USERNAME,
 )
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 from homeassistant.util import get_local_ip
 
 from .const import (
-    DEFAULT_PROFILES,
-    DEFAULT_HOST,
-    DEFAULT_USERNAME,
-    DEFAULT_PORT,
-    DOMAIN,
-    SERVICE_RECONNECT,
-    SERVICE_REBOOT,
-    SUPPORTED_DOMAINS,
-    CONF_USE_WIFI,
-    CONF_USE_PROFILES,
+    ATTR_HOST,
+    CONF_PROFILES,
     CONF_USE_DEFLECTIONS,
     CONF_USE_PORT,
-    CONF_PROFILES,
-    DEFAULT_USE_WIFI,
-    DEFAULT_USE_PROFILES,
+    CONF_USE_PROFILES,
+    CONF_USE_WIFI,
+    DATA_FRITZ_TOOLS_INSTANCE,
+    DEFAULT_HOST,
+    DEFAULT_PORT,
+    DEFAULT_PROFILES,
     DEFAULT_USE_DEFLECTIONS,
     DEFAULT_USE_PORT,
+    DEFAULT_USE_PROFILES,
+    DEFAULT_USE_WIFI,
+    DEFAULT_USERNAME,
+    DOMAIN,
     ERROR_CONNECTION_ERROR,
     ERROR_CONNECTION_ERROR_PROFILES,
     ERROR_PROFILE_NOT_FOUND,
+    SERVICE_REBOOT,
+    SERVICE_RECONNECT,
+    SUPPORTED_DOMAINS,
 )
-
-REQUIREMENTS = ["fritzconnection==1.2.0", "fritz-switch-profiles==1.0.0", "xmltodict==0.12.0"]
-
-DATA_FRITZ_TOOLS_INSTANCE = "fritzbox_tools_instance"
-ATTR_HOST = "host"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -73,7 +67,9 @@ CONFIG_SCHEMA = vol.Schema(
                                     vol.Optional(CONF_PORT): cv.port,
                                     vol.Required(CONF_USERNAME): cv.string,
                                     vol.Required(CONF_PASSWORD): cv.string,
-                                    vol.Optional(CONF_PROFILES): vol.All(cv.ensure_list, [cv.string]),
+                                    vol.Optional(CONF_PROFILES): vol.All(
+                                        cv.ensure_list, [cv.string]
+                                    ),
                                     vol.Optional(CONF_USE_PROFILES): cv.string,
                                     vol.Optional(CONF_USE_PORT): cv.string,
                                     vol.Optional(CONF_USE_WIFI): cv.string,
@@ -92,8 +88,9 @@ CONFIG_SCHEMA = vol.Schema(
 
 SERVICE_SCHEMA = vol.Schema({vol.Required(ATTR_HOST): cv.string})
 
+
 async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
-    """Setup FRITZ!Box Tools component"""
+    """Set up FRITZ!Box Tools component."""
     if DOMAIN in config:
         for entry_config in config[DOMAIN][CONF_DEVICES]:
             hass.async_create_task(
@@ -106,7 +103,7 @@ async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
 
 
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
-    """Setup fritzboxtools from config entry"""
+    """Set up fritzboxtools from config entry."""
     _LOGGER.debug("Setting up FRITZ!Box Tools component")
     host = entry.data.get(CONF_HOST, DEFAULT_HOST)
     port = entry.data.get(CONF_PORT, DEFAULT_PORT)
@@ -118,17 +115,19 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
     use_port = entry.data.get(CONF_USE_PORT, DEFAULT_USE_PORT)
     use_deflections = entry.data.get(CONF_USE_DEFLECTIONS, DEFAULT_USE_DEFLECTIONS)
 
-    fritz_tools = await hass.async_add_executor_job(lambda: FritzBoxTools(
-        host=host,
-        port=port,
-        username=username,
-        password=password,
-        profile_list=profile_list,
-        use_wifi=use_wifi,
-        use_deflections=use_deflections,
-        use_port=use_port,
-        use_profiles=use_profiles,
-    ))
+    fritz_tools = await hass.async_add_executor_job(
+        lambda: FritzBoxTools(
+            host=host,
+            port=port,
+            username=username,
+            password=password,
+            profile_list=profile_list,
+            use_wifi=use_wifi,
+            use_deflections=use_deflections,
+            use_port=use_port,
+            use_profiles=use_profiles,
+        )
+    )
 
     success, error = await hass.async_add_executor_job(fritz_tools.is_ok)
     if not success and error is ERROR_CONNECTION_ERROR:
@@ -143,7 +142,6 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
         return False
 
     hass.data.setdefault(DOMAIN, {DATA_FRITZ_TOOLS_INSTANCE: {}, CONF_DEVICES: set()})
-    hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE][host] = fritz_tools
     hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE][entry.entry_id] = fritz_tools
 
     setup_hass_services(hass)
@@ -161,33 +159,43 @@ def setup_hass_services(hass):
     """Home Assistant services."""
 
     def reboot(call):
-        """Reboot fritzbox"""
+        """Reboot fritzbox."""
         host = call.data.get(ATTR_HOST)
-        fritztools = hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE].get(host,None)
+        fritztools = hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE].get(host, None)
         if fritztools is None:
-            _LOGGER.error(f"{SERVICE_REBOOT}: Please supply a valid hostname of a configured fritzbox for the service (e.g. 192.168.178.1)")
+            _LOGGER.error(
+                f"{SERVICE_REBOOT}: Please supply a valid hostname of a configured fritzbox for the service (e.g. 192.168.178.1)"
+            )
         else:
             fritztools.service_reboot_fritzbox()
 
     def reconnect(call):
-        """Reboot fritzbox"""
+        """Reconnect fritzbox."""
         host = call.data.get(ATTR_HOST)
-        fritztools = hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE].get(host,None)
+        fritztools = hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE].get(host, None)
         if fritztools is None:
-            _LOGGER.error(f"{SERVICE_RECONNECT}: Please supply a valid hostname of a configured fritzbox for the service (e.g. 192.168.178.1)")
+            _LOGGER.error(
+                f"{SERVICE_RECONNECT}: Please supply a valid hostname of a configured fritzbox for the service (e.g. 192.168.178.1)"
+            )
         else:
             fritztools.service_reconnect_fritzbox()
 
     hass.services.async_register(
-        DOMAIN,SERVICE_RECONNECT, reconnect, schema=SERVICE_SCHEMA,
+        DOMAIN,
+        SERVICE_RECONNECT,
+        reconnect,
+        schema=SERVICE_SCHEMA,
     )
     hass.services.async_register(
-        DOMAIN, SERVICE_REBOOT, reboot, schema=SERVICE_SCHEMA,
+        DOMAIN,
+        SERVICE_REBOOT,
+        reboot,
+        schema=SERVICE_SCHEMA,
     )
+
 
 async def async_unload_entry(hass: HomeAssistantType, entry: ConfigType) -> bool:
     """Unload FRITZ!Box Tools config entry."""
-    hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE].pop(entry.data.get(CONF_HOST))
     hass.data[DOMAIN][DATA_FRITZ_TOOLS_INSTANCE].pop(entry.entry_id)
     hass.services.async_remove(DOMAIN, SERVICE_RECONNECT)
     hass.services.async_remove(DOMAIN, SERVICE_REBOOT)
@@ -198,29 +206,32 @@ async def async_unload_entry(hass: HomeAssistantType, entry: ConfigType) -> bool
     return True
 
 
-class FritzBoxTools(object):
+class FritzBoxTools:
+    """FrtizBoxTools class."""
+
     """
     Attention: The initialization of the class performs sync I/O. If you're calling this from within Home Assistant,
     wrap it in await self.hass.async_add_executor_job(lambda: FritzBoxTools(...))
     """
 
     def __init__(
-            self,
-            password,
-            username=DEFAULT_USERNAME,
-            host=DEFAULT_HOST,
-            port=DEFAULT_PORT,
-            profile_list=DEFAULT_PROFILES,
-            use_port=DEFAULT_USE_PORT,
-            use_deflections=DEFAULT_USE_DEFLECTIONS,
-            use_wifi=DEFAULT_USE_WIFI,
-            use_profiles=DEFAULT_USE_PROFILES,
+        self,
+        password,
+        username=DEFAULT_USERNAME,
+        host=DEFAULT_HOST,
+        port=DEFAULT_PORT,
+        profile_list=DEFAULT_PROFILES,
+        use_port=DEFAULT_USE_PORT,
+        use_deflections=DEFAULT_USE_DEFLECTIONS,
+        use_wifi=DEFAULT_USE_WIFI,
+        use_profiles=DEFAULT_USE_PROFILES,
     ):
+        """Initialize FritzboxTools class."""
         # pylint: disable=import-error
         from fritzconnection import FritzConnection
+        from fritzconnection.core.exceptions import FritzConnectionException
         from fritzconnection.lib.fritzstatus import FritzStatus
         from fritzprofiles import FritzProfileSwitch
-        from fritzconnection.core.exceptions import FritzConnectionException
 
         # general timeout for all requests to the router. Some calls need quite some time.
 
@@ -229,9 +240,12 @@ class FritzBoxTools(object):
                 address=host, port=port, user=username, password=password, timeout=60.0
             )
             if profile_list != DEFAULT_PROFILES:
-                self.profile_switch = {profile: FritzProfileSwitch(
-                    "http://" + host, username, password, profile
-                ) for profile in profile_list}
+                self.profile_switch = {
+                    profile: FritzProfileSwitch(
+                        "http://" + host, username, password, profile
+                    )
+                    for profile in profile_list
+                }
             else:
                 self.profile_switch = {}
 
@@ -266,29 +280,36 @@ class FritzBoxTools(object):
         self.use_profiles = use_profiles
 
     def service_reconnect_fritzbox(self) -> None:
+        """Define service reconnect."""
         _LOGGER.info("Reconnecting the fritzbox.")
         self.connection.reconnect()
 
     def service_reboot_fritzbox(self) -> None:
+        """Define service reboot."""
         _LOGGER.info("Rebooting the fritzbox.")
         self.connection.call_action("DeviceConfig1", "Reboot")
 
     def is_ok(self):
+        """Return status."""
         return self.success, self.error
 
     @property
     def unique_id(self):
+        """Return unique id."""
         return self._unique_id
 
     @property
     def fritzbox_model(self):
+        """Return model."""
         return self._device_info["model"].replace("FRITZ!Box ", "")
 
     @property
     def device_info(self):
+        """Return device info."""
         return self._device_info
 
     def _fetch_device_info(self):
+        """Fetch device info."""
         info = self.connection.call_action("DeviceInfo:1", "GetInfo")
         return {
             "identifiers": {
